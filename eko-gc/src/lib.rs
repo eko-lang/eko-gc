46ee770d1867121e65bf0b911470cf39b53665d0
Ravern Koh <ravernkoh@gmail.com>
2019-02-17 19:04:24 +0800

    Implement `Clone` and `Debug` (#4)
    * Derive `Clone` and implement `Debug`
    
    * Implement `Debug` for `RefCell`
    
    * Create tests
    
    * Rename `marker` fields to `phantom`

--- eko-gc/src/lib.rs
+++ eko-gc/src/lib.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
@@ -7,28 +8,43 @@ pub use self::trace::Trace;
 mod trace;
 
 pub struct Arena<'gc> {
-    marker: PhantomData<&'gc ()>,
+    phantom: PhantomData<&'gc ()>,
 }
 
 impl<'gc> Arena<'gc> {
     pub fn new() -> Arena<'gc> {
         Arena {
-            marker: PhantomData,
+            phantom: PhantomData,
         }
     }
 }
 
 pub struct Gc<'gc, T: Trace + ?Sized + 'gc> {
     data: Rc<T>,
-    marker: PhantomData<&'gc ()>,
+    phantom: PhantomData<&'gc ()>,
 }
 
 impl<'gc, T: Trace + 'gc> Gc<'gc, T> {
     pub fn new(_arena: &Arena<'gc>, data: T) -> Gc<'gc, T> {
         Gc {
             data: Rc::new(data),
-            marker: PhantomData,
+            phantom: PhantomData,
         }
+    }
+}
+
+impl<'gc, T: Trace + ?Sized + 'gc> Clone for Gc<'gc, T> {
+    fn clone(&self) -> Gc<'gc, T> {
+        Gc {
+            data: self.data.clone(),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'gc, T: fmt::Debug + Trace + ?Sized + 'gc> fmt::Debug for Gc<'gc, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(&**self, f)
     }
 }
 
@@ -43,7 +59,7 @@ impl<'gc, T: Trace + ?Sized + 'gc> Deref for Gc<'gc, T> {
 unsafe impl<'gc, T: Trace + 'gc> Trace for Gc<'gc, T> {}
 
 pub struct RefCell<'gc, T: Trace + ?Sized + 'gc> {
-    marker: PhantomData<&'gc ()>,
+    phantom: PhantomData<&'gc ()>,
     data: std::cell::RefCell<T>,
 }
 
@@ -51,7 +67,30 @@ impl<'gc, T: Trace + 'gc> RefCell<'gc, T> {
     pub fn new(_arena: &Arena<'gc>, data: T) -> RefCell<'gc, T> {
         RefCell {
             data: std::cell::RefCell::new(data),
-            marker: PhantomData,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'gc, T: fmt::Debug + Trace + ?Sized + 'gc> fmt::Debug for RefCell<'gc, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.data.try_borrow() {
+            Ok(borrow) => f.debug_struct("RefCell").field("value", &borrow).finish(),
+            Err(_) => {
+                // The RefCell is mutably borrowed so we can't look at its value
+                // here. Show a placeholder instead.
+                struct BorrowedPlaceholder;
+
+                impl fmt::Debug for BorrowedPlaceholder {
+                    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                        f.write_str("<borrowed>")
+                    }
+                }
+
+                f.debug_struct("RefCell")
+                    .field("value", &BorrowedPlaceholder)
+                    .finish()
+            }
         }
     }
 }
@@ -60,14 +99,14 @@ impl<'gc, T: Trace + ?Sized + 'gc> RefCell<'gc, T> {
     pub fn borrow<'a>(&'a self) -> Ref<'a, 'gc, T> {
         Ref {
             data: self.data.borrow(),
-            marker: PhantomData,
+            phantom: PhantomData,
         }
     }
 
     pub fn borrow_mut<'a>(&'a self) -> RefMut<'a, 'gc, T> {
         RefMut {
             data: self.data.borrow_mut(),
-            marker: PhantomData,
+            phantom: PhantomData,
         }
     }
 }
@@ -76,7 +115,7 @@ unsafe impl<'gc, T: Trace + ?Sized + 'gc> Trace for RefCell<'gc, T> {}
 
 pub struct Ref<'a, 'gc, T: Trace + ?Sized + 'gc> {
     data: std::cell::Ref<'a, T>,
-    marker: PhantomData<&'gc ()>,
+    phantom: PhantomData<&'gc ()>,
 }
 
 impl<'a, 'gc, T: Trace + ?Sized + 'gc> Deref for Ref<'a, 'gc, T> {
@@ -89,7 +128,7 @@ impl<'a, 'gc, T: Trace + ?Sized + 'gc> Deref for Ref<'a, 'gc, T> {
 
 pub struct RefMut<'a, 'gc, T: Trace + ?Sized + 'gc> {
     data: std::cell::RefMut<'a, T>,
-    marker: PhantomData<&'gc ()>,
+    phantom: PhantomData<&'gc ()>,
 }
 
 impl<'a, 'gc, T: Trace + ?Sized + 'gc> Deref for RefMut<'a, 'gc, T> {
@@ -118,6 +157,14 @@ mod tests {
     }
 
     #[test]
+    fn gc_clone() {
+        let arena = Arena::new();
+        let gc = Gc::new(&arena, 0);
+        let gc_clone = Gc::clone(&gc);
+        assert_eq!(*gc_clone, 0);
+    }
+
+    #[test]
     fn ref_cell() {
         let arena = Arena::new();
         let ref_cell = RefCell::new(&arena, 0);
@@ -132,6 +179,16 @@ mod tests {
         let gc = Gc::new(&arena, RefCell::new(&arena, 0));
         assert_eq!(*gc.borrow(), 0);
         *gc.borrow_mut() = 1;
+        assert_eq!(*gc.borrow(), 1);
+    }
+
+    #[test]
+    fn gc_ref_cell_clone() {
+        let arena = Arena::new();
+        let gc = Gc::new(&arena, RefCell::new(&arena, 0));
+        assert_eq!(*gc.borrow(), 0);
+        let gc_clone = Gc::clone(&gc);
+        *gc_clone.borrow_mut() = 1;
         assert_eq!(*gc.borrow(), 1);
     }
 }
