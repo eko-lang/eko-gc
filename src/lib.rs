6afa5ab2cf483b1cddff0572674eac685e859f9d
Ravern Koh <ravernkoh@gmail.com>
2019-02-17 12:56:02 +0800

    Build initial implementation (#1)
    * Build initial implementation
    
    * Create tests for `Gc` and `RefCell`
    
    * Make `Gc::new` public
    
    * Implement `Trace` for common types

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,137 @@
+use std::marker::PhantomData;
+use std::ops::{Deref, DerefMut};
+use std::rc::Rc;
 
+pub use self::trace::Trace;
+
+mod trace;
+
+pub struct Arena<'gc> {
+    marker: PhantomData<&'gc ()>,
+}
+
+impl<'gc> Arena<'gc> {
+    pub fn new() -> Arena<'gc> {
+        Arena {
+            marker: PhantomData,
+        }
+    }
+}
+
+pub struct Gc<'gc, T: Trace + ?Sized + 'gc> {
+    data: Rc<T>,
+    marker: PhantomData<&'gc ()>,
+}
+
+impl<'gc, T: Trace + 'gc> Gc<'gc, T> {
+    pub fn new(_arena: &Arena<'gc>, data: T) -> Gc<'gc, T> {
+        Gc {
+            data: Rc::new(data),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<'gc, T: Trace + ?Sized + 'gc> Deref for Gc<'gc, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.data.deref()
+    }
+}
+
+unsafe impl<'gc, T: Trace + 'gc> Trace for Gc<'gc, T> {}
+
+pub struct RefCell<'gc, T: Trace + ?Sized + 'gc> {
+    marker: PhantomData<&'gc ()>,
+    data: std::cell::RefCell<T>,
+}
+
+impl<'gc, T: Trace + 'gc> RefCell<'gc, T> {
+    pub fn new(_arena: &Arena<'gc>, data: T) -> RefCell<'gc, T> {
+        RefCell {
+            data: std::cell::RefCell::new(data),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<'gc, T: Trace + ?Sized + 'gc> RefCell<'gc, T> {
+    pub fn borrow<'a>(&'a self) -> Ref<'a, 'gc, T> {
+        Ref {
+            data: self.data.borrow(),
+            marker: PhantomData,
+        }
+    }
+
+    pub fn borrow_mut<'a>(&'a self) -> RefMut<'a, 'gc, T> {
+        RefMut {
+            data: self.data.borrow_mut(),
+            marker: PhantomData,
+        }
+    }
+}
+
+unsafe impl<'gc, T: Trace + ?Sized + 'gc> Trace for RefCell<'gc, T> {}
+
+pub struct Ref<'a, 'gc, T: Trace + ?Sized + 'gc> {
+    data: std::cell::Ref<'a, T>,
+    marker: PhantomData<&'gc ()>,
+}
+
+impl<'a, 'gc, T: Trace + ?Sized + 'gc> Deref for Ref<'a, 'gc, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.data.deref()
+    }
+}
+
+pub struct RefMut<'a, 'gc, T: Trace + ?Sized + 'gc> {
+    data: std::cell::RefMut<'a, T>,
+    marker: PhantomData<&'gc ()>,
+}
+
+impl<'a, 'gc, T: Trace + ?Sized + 'gc> Deref for RefMut<'a, 'gc, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.data.deref()
+    }
+}
+
+impl<'a, 'gc, T: Trace + ?Sized + 'gc> DerefMut for RefMut<'a, 'gc, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        self.data.deref_mut()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Arena, Gc, RefCell};
+
+    #[test]
+    fn gc() {
+        let arena = Arena::new();
+        let gc = Gc::new(&arena, 0);
+        assert_eq!(*gc, 0);
+    }
+
+    #[test]
+    fn ref_cell() {
+        let arena = Arena::new();
+        let ref_cell = RefCell::new(&arena, 0);
+        assert_eq!(*ref_cell.borrow(), 0);
+        *ref_cell.borrow_mut() = 1;
+        assert_eq!(*ref_cell.borrow(), 1);
+    }
+
+    #[test]
+    fn gc_ref_cell() {
+        let arena = Arena::new();
+        let gc = Gc::new(&arena, RefCell::new(&arena, 0));
+        assert_eq!(*gc.borrow(), 0);
+        *gc.borrow_mut() = 1;
+        assert_eq!(*gc.borrow(), 1);
+    }
+}
---
+++ src/trace.rs
@@ -0,0 +1,31 @@
+use std::collections::{BTreeMap, HashMap};
+
+pub unsafe trait Trace {}
+
+macro_rules! unsafe_empty_trace {
+    ($type:ident) => {
+        unsafe impl Trace for $type {}
+    };
+}
+
+unsafe_empty_trace!(bool);
+unsafe_empty_trace!(i8);
+unsafe_empty_trace!(i16);
+unsafe_empty_trace!(i32);
+unsafe_empty_trace!(i64);
+unsafe_empty_trace!(i128);
+unsafe_empty_trace!(u8);
+unsafe_empty_trace!(u16);
+unsafe_empty_trace!(u32);
+unsafe_empty_trace!(u64);
+unsafe_empty_trace!(f32);
+unsafe_empty_trace!(f64);
+unsafe_empty_trace!(String);
+
+unsafe impl<K: Trace, V: Trace> Trace for BTreeMap<K, V> {}
+
+unsafe impl<K: Trace, V: Trace> Trace for HashMap<K, V> {}
+
+unsafe impl<T: Trace> Trace for Vec<T> {}
+
+unsafe impl<T: Trace> Trace for Option<T> {}
